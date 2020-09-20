9a2c5ea887c9b6cdc281e3fc09c1744148f89f15
Stefan Evanghelides <stefan.evanghelides@cadmatic.com>
2020-09-20 10:35:09 +0200

    added script to migrate the schema, improved the schemas and added missing volumes from the scripts

--- utils/volumes/create-volumes.sh
+++ utils/volumes/create-volumes.sh
@@ -5,6 +5,7 @@ docker volume create cassandra-schema
 docker volume create cassandra-data-seed
 docker volume create cassandra-data-1
 docker volume create cassandra-data-2
+docker volume create historical-data
 docker volume create kafka-offset
 docker volume create redis-data
 docker volume create redis-conf

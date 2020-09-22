640ed89d8b3e355cae725fe636c7f3e68897ffda
Stefan Evanghelides <stefan.evanghelides@cadmatic.com>
2020-09-22 14:08:51 +0200

    updated the server in docker-compose, some updates on mongodb

--- utils/volumes/create-volumes.sh
+++ utils/volumes/create-volumes.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 # Create the volumes used in docker-compose
+docker volume create mongo-schema
+docker volume create mongodb-seed-data
+docker volume create mongo-historical-data
 docker volume create cassandra-schema
 docker volume create cassandra-data-seed
 docker volume create cassandra-data-1
 docker volume create cassandra-data-2
-docker volume create historical-data
+docker volume create cassandra-historical-data
 docker volume create kafka-offset
 docker volume create redis-data
 docker volume create redis-conf

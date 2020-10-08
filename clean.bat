b0360fb9fc9b803bed9b12a56d3d913f6c3dbdd3
Stefan Evanghelides <stefan.evanghelides@cadmatic.com>
2020-10-08 21:41:24 +0200

    added cleaning scripts

---
+++ clean.bat
@@ -0,0 +1,8 @@
+@echo off
+
+:: Kill running containers
+docker container kill server mongo-seed ui kafka zookeeper cassandra-1 cassandra-2 cassandra-seed
+
+:: Clean the system and volume
+docker system prune -f
+docker volume prune -f
